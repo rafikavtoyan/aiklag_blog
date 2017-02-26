de2eb939165ca26afea381b1b5637ffc73525828
Rafik Avtoyan <rafikavtoyan@gmail.com>
2017-02-27 00:37:14 +0400

    Add basic functional for post app.

--- aiklag_blog/aiklag_blog/settings.py
+++ aiklag_blog/aiklag_blog/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'aiklag_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    #'/var/www/static/',
+]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+
--- aiklag_blog/posts/views.py
+++ aiklag_blog/posts/views.py
@@ -1,3 +1,91 @@
-from django.shortcuts import render
+from urllib import quote_plus
+from django.contrib import messages
+from django.http import HttpResponseRedirect, Http404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import PostForm
 
 # Create your views here.
+from .models import Post
+
+
+def post_create(request):
+    if not request.user.is_authenticated():
+        raise Http404
+    form = PostForm(request.POST or None, request.FILES or None)
+    if request.POST:
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.user = request.user
+            instance.save()
+            messages.success(request, "Successfully Created")
+            return HttpResponseRedirect(instance.get_absolute_url())
+        else:
+            messages.error(request, "failed to create post")
+
+    context = {
+        "form": form
+    }
+    return render(request, "post_form.html", context)
+
+
+def post_detail(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
+    share_string = quote_plus(instance.content)
+    context = {
+        "title": "detail",
+        "instance": instance,
+        "share_string": share_string,
+    }
+    return render(request, "post_detail.html", context)
+
+
+def post_list(request):
+    queryset_list = Post.objects.active() #.order_by("-timestamp")
+    paginator = Paginator(queryset_list, 2) # Show 25 contacts per page
+    page_request_var = "page"
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+    context = {
+        "object_list": queryset,
+        "title": "list",
+        "page_request_var": page_request_var
+    }
+    return render(request, "posts_list.html", context)
+
+
+def post_update(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
+    if request.POST:
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save()
+            messages.success(request, "Successfully updated", extra_tags="some_extra_tag")
+            return HttpResponseRedirect(instance.get_absolute_url())
+        else:
+            messages.error(request, "failed to update post")
+
+    context = {
+        "title": "detail",
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
+
+
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Successfully deleted", extra_tags="some_extra_tag")
+    return redirect("posts:list")
+
+
+
