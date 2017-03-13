8a7d18764eae5e222edb1182a74420454d23f7c0
Rafik Avtoyan <rafikavtoyan@gmail.com>
2017-03-13 23:34:27 +0400

    Add comments forms and comments to post relations.

--- aiklag_blog/posts/models.py
+++ aiklag_blog/posts/models.py
@@ -9,6 +9,7 @@ from django.utils import timezone
 from markdown_deux import markdown
 from django.utils.safestring import mark_safe
 from comments.models import Comment
+from django.contrib.contenttypes.models import ContentType
 
 
 def upload_location(instance, filename):
@@ -63,6 +64,13 @@ class Post(models.Model):
         instance = self
         qs = Comment.objects.filter_by_instance(instance)
         return qs
+
+    @property
+    def get_content_type(self):
+        instance = self
+        content_type = ContentType.objects.get_for_model(instance.__class__)
+        return content_type
+
 
 def create_slug(instance, new_slug=None):
     slug = slugify(instance.title)
--- aiklag_blog/posts/views.py
+++ aiklag_blog/posts/views.py
@@ -4,10 +4,13 @@ from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import PostForm
+from comments.forms import CommentForm
 
 # Create your views here.
 from .models import Post
 from comments.models import Comment
+from django.contrib.contenttypes.models import ContentType
+
 
 def post_create(request):
     if not request.user.is_authenticated():
@@ -32,12 +35,31 @@ def post_create(request):
 def post_detail(request, slug=None):
     instance = get_object_or_404(Post, slug=slug)
     share_string = quote_plus(instance.content)
+
+    initial_data = {
+        "content_type": instance.get_content_type,
+        "object_id": instance.id
+    }
+    form = CommentForm(request.POST or None, initial=initial_data)
+    if form.is_valid():
+        c_type = form.cleaned_data.get("content_type")
+        content_type = ContentType.objects.get(model=c_type)
+        obj_id = form.cleaned_data.get("object_id")
+        content_data = form.cleaned_data.get("content")
+        new_comment, created = Comment.objects.get_or_create(
+            user=request.user,
+            content_type=content_type,
+            object_id=obj_id,
+            content=content_data
+        )
+
     comments = instance.comments
     context = {
         "title": "detail",
         "instance": instance,
         "share_string": share_string,
-        "comments": comments
+        "comments": comments,
+        "comment_form": form
     }
 
     return render(request, "post_detail.html", context)
